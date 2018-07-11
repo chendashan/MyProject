771c4717afb9d7d0ee485a870c9f5459a14dc5f4
chendashan <chends3@163.com>
2018-07-11 16:04:35 +0800

    完善功能1

--- app/src/main/java/com/example/user/myproject/MainActivity.java
+++ app/src/main/java/com/example/user/myproject/MainActivity.java
@@ -29,7 +29,7 @@ public class MainActivity extends AppCompatActivity {
         btFeature1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast.makeText(MainActivity.this, "功能分支1", Toast.LENGTH_SHORT).show();
+                Toast.makeText(MainActivity.this, "这是功能分支1", Toast.LENGTH_SHORT).show();
             }
         });
     }
